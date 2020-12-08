c524dad930d9acd3b7abbe7064926d8393c25ca7
Ananya3ap27 <71383475+Ananya3ap27@users.noreply.github.com>
2020-12-08 15:07:38 +0530

    Add files via upload

---
+++ Constraint.js
@@ -0,0 +1,21 @@
+class Constrant{
+    constructor(a,b,offsetx,offsety){
+        this.bx=offsetx;
+        this.by=offsety;
+        var option= {
+            bodyA:a.body,
+            bodyB:b.body,
+            pointB:{x:this.bx,y:this.by},
+            stiffness:1.9,
+            length:300
+        }
+        this.rope=Constraint.create(option);
+        World.add(world,this.rope);
+    }
+    attach(body){
+		this.rope.bodyA=body;
+	}
+    display(){
+        
+    }
+}
